d48f9e696e1609c13593c49085d279c550d5f3a1
mnishig <mnishig@gmail.com>
2018-11-18 21:07:13 +0900

    add .eslint.js eslint rule

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "browser": false,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2016
+    },
+    "rules": {
+        "indent": [
+          "error",
+          2,
+          { "SwitchCase": 1}
+        ],
+        "linebreak-style": [
+          "error",
+          "unix"
+        ],
+        "quotes": [
+          "error",
+          "single"
+        ],
+        "semi": [
+          "error",
+          "always"
+        ],
+      "strict": 0,
+      "no-var": 2,
+      "no-console": 0
+    }
+};
+
